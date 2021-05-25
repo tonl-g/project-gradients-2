bd98f1c21f1a546a19143b9c9096191235bde06f
Imo999 <rcajina.pro@gmail.com>
2021-05-25 21:44:08 +0200

    Add new component Full screen button

---
+++ src/components/GradientFullscreenButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GradientFullscreenButton = () => {
+  return (
+    <a className="btn btn-outline-dark w-100" href="/">
+      Plein écran
+    </a>
+  )
+}
+
+export default GradientFullscreenButton
+
+
+// Need router pour continuer
