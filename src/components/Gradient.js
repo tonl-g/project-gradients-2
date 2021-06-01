62663e44b838104a2374a271233113c91a29186b
Imo999 <rcajina.pro@gmail.com>
2021-06-01 11:34:55 +0200

    update final

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -18,25 +18,3 @@ const Gradient = ({ colorStart, colorEnd, name, tags, id }) => {
 }
 
 export default Gradient
-
-/* import GradientTitle from './GradientTitle'
-import GradientPill from './GradientPill'
-import GradientCode from './GradientCode'
-import GradientTags from './GradientTags'
-import GradientFullscreenButton from './GradientFullscreenButton'
-
-const Gradient = ({ colorStart, colorEnd, name, tags, filter, setFilter }) => {
-  return (
-    <li className="col-lg-3 col-md-4 col-sm-6">
-      <div className="card p-3 mb-4 shadow">
-        <GradientPill colorStart={colorStart} colorEnd={colorEnd} />
-        <GradientTitle>{name}</GradientTitle>
-        <GradientCode colorStart={colorStart} colorEnd={colorEnd} />
-        <GradientTags tags={tags} filter={filter} setFilter={setFilter} />
-        <GradientFullscreenButton  />
-      </div>
-    </li>
-  )
-}
-
-export default Gradient */
